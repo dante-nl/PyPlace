fb1c4aab48e5c9e96c490647e178d8c95f4b7a6e
dante_nl <61943433+dante-nl@users.noreply.github.com>
2023-06-11 17:21:10 +0200

    Update download.py

--- Orders/gYvGA/download.py
+++ Orders/gYvGA/download.py
@@ -123,7 +123,7 @@ def log(message):
 
 def UpdateCheck():
 	log("Checking for latest version...")
-	response = requests.get("https://pyplace.dantenl.tk/version.json")
+	response = requests.get("https://pyplace.dantenl.com/version.json")
 	if response.status_code != 200:
 		print(f"{bcolors.FAIL}Error:{bcolors.END} Could not check for updates! Response code: {response.status_code}")
 		return
@@ -153,7 +153,7 @@ def UpdateCheck():
 					f"{bcolors.INFO}Downloading latest version of PyPlace...{bcolors.END}")
 				log("Retrieving latest version of PyPlace...")
 				r = requests.get(
-					"https://pyplace.dantenl.tk/PyPlace-Latest.py", allow_redirects=True)
+					"https://pyplace.dantenl.com/PyPlace-Latest.py", allow_redirects=True)
 				if not r.ok:
 					print(f"{bcolors.FAIL}Error:{bcolors.END} Could not get the PyPlace file! Status code: {r.status_code}")
 					return
@@ -336,7 +336,7 @@ URLToPythonFile)
 			else:
 				print(f"{bcolors.FAIL}Error:{bcolors.END} That does not appear to be a valid URL!")
 		elif Answer4 == "2":
-			StoreRequest = requests.get("https://pyplace.dantenl.tk/store.json", allow_redirects=False)
+			StoreRequest = requests.get("https://pyplace.dantenl.com/store.json", allow_redirects=False)
 			if not StoreRequest.ok:
 				print(f"{bcolors.FAIL}Error:{bcolors.END} Could not connect to the PyPlace store! Response code: {StoreRequest.status_code}")
 				return
@@ -431,7 +431,7 @@ URLToPythonFile)
 		elif Answer4 == "3":
 
 			ExperimentRequest = requests.get(
-				"https://pyplace.dantenl.tk/experiments.json", allow_redirects=True)
+				"https://pyplace.dantenl.com/experiments.json", allow_redirects=True)
 			if not ExperimentRequest.ok:
 				print(f"{bcolors.FAIL}Error:{bcolors.END} Could not connect to the PyPlace Experiment Store! Response code: {ExperimentRequest.status_code}")
 				return
@@ -659,7 +659,7 @@ to open the PyPlace Expirements Store!
 						print(f"{bcolors.INFO}Downloading latest version of PyPlace...{bcolors.END}")
 						log("Retrieving latest version of PyPlace...")
 						r = requests.get(
-							"https://pyplace.dantenl.tk/PyPlace-Latest.py", allow_redirects=True)
+							"https://pyplace.dantenl.com/PyPlace-Latest.py", allow_redirects=True)
 						if not r.ok:
 							print(
 								f"{bcolors.FAIL}Error:{bcolors.END} Could not get the PyPlace file! Status code: {r.status_code}")
@@ -752,7 +752,7 @@ def PyPlaceRegular():
 			if Answer3.lower().startswith("-install"):
 				order_name = Answer3.lower().replace("-install ", "")
 				log("Looking up order...")
-				response = requests.get(f"https://pyplace.dantenl.tk/orders/{order_name}/manifest.json")
+				response = requests.get(f"https://pyplace.dantenl.com/orders/{order_name}/manifest.json")
 				if response.status_code == 404:
 					print(f"{bcolors.FAIL}Error:{bcolors.END} That order does not exist.")
 					return
