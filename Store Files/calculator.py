37c0ed3f5d85798a773ddd43521d67f7010254f6
dante_nl <61943433+dante-nl@users.noreply.github.com>
2023-06-17 13:58:03 +0200

    Calculator is now more advanced

--- Store Files/calculator.py
+++ Store Files/calculator.py
@@ -1,3 +1,5 @@
+import ast
+import operator as op
 import sys
 
 logo = """
@@ -18,66 +20,95 @@ logo = """
 """
 
 
-def add(n1, n2):
-	return n1 + n2
+operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
+             ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
+             ast.USub: op.neg}
 
-def subtract(n1, n2):
-	return n1 - n2
 
-def multiply(n1, n2):
-	return n1 * n2
+def calc(expr):
+	"""Does a calculation, for example:
+	```py
+	calc("3*3") # 9
+	```"""
+	return eval_(ast.parse(expr, mode='eval').body)
 
-def divide(n1, n2):
-	return n1 / n2
 
-operators = {
-	"+": add, 
-	"-": subtract, 
-	"*": multiply, 
-	"/": divide
-}
+def eval_(node):
+	if isinstance(node, ast.Num):  # <number>
+		return node.n
+	elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+		return operators[type(node.op)](eval_(node.left), eval_(node.right))
+	elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+		return operators[type(node.op)](eval_(node.operand))
+	else:
+		raise TypeError(node)
+	
+class colors:
+	LOG = '\033[95m'
+	INFO = '\033[94m'
+	OKCYAN = '\033[96m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	END = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+
+def log(text):
+	"""Sends a log message. Usage:
+	```py
+	log("Hello!")
+	```"""
+	print(f"{colors.LOG}Log:{colors.END} {text}")
+def info(text):
+	"""Sends an info message. Usage:
+	```py
+	info("Hello!")
+	```"""
+	print(f"{colors.INFO}Info:{colors.END} {text}")
+
+def ok1(text):
+	"""Sends a cyan OK message. Usage:
+	```py
+	ok1("Hello!")
+	```"""
+	print(f"{colors.OKCYAN}Success:{colors.END} {text}")
+
+def ok2(text):
+	"""Sends a green OK message. Usage:
+	```py
+	ok2("Hello!")
+	```"""
+	print(f"{colors.OKGREEN}Success:{colors.END} {text}")
+
+def warning(text):
+	"""Sends a warning message. Usage:
+	```py
+	warning("Hello!")
+	```"""
+	print(f"{colors.WARNING}Warning:{colors.END} {text}")
+
+def error(text):
+	"""Sends an error message. Usage:
+	```py
+	error("Hello!")
+	```"""
+	print(f"{colors.FAIL}Error:{colors.END} {text}")
 
 print(logo)
-def calculator():
-	num1 = float(input("What is the first number? "))
-	for operator in operators:
-		print(operator)
-	
-	should_continue = True
-	while should_continue == True:
-		
-		operator = input("Pick an operation: ")
-		
-		num2 = float(input("What is the next number? "))
-		
-		result = operators[operator](num1, num2)
-		print(f"{num1} {operator} {num2} = {result}")
 
-		InvalidAnswer = True
-		while InvalidAnswer == True:
-			continue_ = input(f"Continue calculating with {result}? (y/n) ")
-			if continue_ == "y":
-				num1 = result
-				InvalidAnswer = False
-			elif continue_ == "n":
-				SecondInvalidAnswer = True
-				while SecondInvalidAnswer == True:
-					exit = input("Do you want to exit the program? (y/n) ")
-					if exit.lower() == "y":
-						InvalidAnswer = False
-						SecondInvalidAnswer = False
-						print("Bye!")
-						sys.exit(0)
-					elif exit.lower() == "n":	
-						should_continue = False
-						InvalidAnswer = False
-						SecondInvalidAnswer = False
-						print()
-						calculator()
-					else:
-						print(f"I'm not sure what you mean with {exit}")
-			else:
-				print(f"I'm not sure what you mean with {continue_}")
+execute_code = True
+last_answer = None
+while execute_code == True:
+	equation = input("Please enter an equation, \e to exit: ").lower()
 
-calculator()
-		
+	if equation == "\e":
+		execute_code = False
+		sys.exit(0)
+
+	try:
+		print(f"{equation} = {calc(equation)}")
+	except ZeroDivisionError:
+		print(f"{equation} = divide by 0 error")
+	except TypeError:
+		print(f"{equation} = error")
